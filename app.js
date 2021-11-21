2683f9ceeb0ea48cacf0e5e3c1051ca8820220ad
Alvi Hasan <livealvi@gmail.com>
2021-11-21 22:15:36 +0600

    9.6 Connect Database Using Mongoose

--- app.js
+++ app.js
@@ -1,56 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
-
-app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/about", (req, res) => {
-  res.render("pages/about", { title: "I am About Page" });
-});
-
-app.get("/help", (req, res) => {
-  res.render("pages/help", { title: "I am Help Page" });
-});
-
 app.get("/", (req, res) => {
-  let post = {
-    title: "Test Title",
-    body: "Test Body",
-    published: true,
-  };
-
-  let posts = [
-    {
-      title: "Test One",
-      author: "Alvi Hasan",
-    },
-    {
-      title: "Test Two",
-      author: "Alvi Hasan",
-    },
-    {
-      title: "Test Three",
-      author: "Alvi Hasan",
-    },
-    {
-      title: "Test Four",
-      author: "Alvi Hasan",
-    },
-  ];
-
-  res.render("pages/index", { title: "EJs is Easy", post, posts });
-});
-
-app.get("*", function (req, res) {
-  res.send("<h1>404 NOT PAGE FOUND</h1>");
+  res.json({ message: "Welcome" });
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING PORT ${PORT}`);
-});
+
+mongoose
+  .connect(
+    `mongodb+srv://livealvi:Highme1@cluster0.amhkf.mongodb.net/test-db?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
