0f3c04827625a201d9c8d3a2f419585ed72bcf44
Alvi Hasan <livealvi@gmail.com>
2021-11-21 19:47:12 +0600

    8.4 Setup EJS

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Working with Template Engine");
+  res.render("index");
 });
 
 app.get("*", function (req, res) {
