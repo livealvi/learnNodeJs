19ba6455340639a9c8d9261a6654289dfa1c38cc
Alvi Hasan <livealvi@gmail.com>
2021-11-21 12:17:54 +0600

    7.3 Contact Model

---
+++ Contacts.js
@@ -0,0 +1,40 @@
+class Contacts {
+  constructor() {
+    this.contacts = [];
+  }
+
+  getAllContacts() {
+    return this.contacts;
+  }
+
+  getAllContactsBuId(id) {
+    return this.contacts.find((contacts) => contact.id == id);
+  }
+
+  createContact(contact) {
+    contact.id = this.contacts.length + 1;
+    this.contacts.push(contact);
+    return contact;
+  }
+
+  updateContactById(id, updatedContact) {
+    let index = this.contact.findIndex((contact) => contact.id == id);
+
+    this.contacts[index.name] =
+      updatedContact.name || this.contacts[index].name;
+    this.contacts[index.phone] =
+      updatedContact.phone || this.contacts[index].phone;
+    this.contacts[index.email] =
+      updatedContact.email || this.contacts[index].email;
+    return this.contacts[index];
+  }
+
+  deleteContactById(id) {
+    let index = this.contact.findIndex((contact) => contact.id == id);
+    let deletedObj = this.contacts[index];
+    this.contacts = this.contact.filter((contact) => contact.id !== id);
+    return deletedObj;
+  }
+}
+
+exports.contacts = new Contact();
