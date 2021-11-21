68546ee149c631ea4066289c6eb2aa0551005887
Alvi Hasan <livealvi@gmail.com>
2021-11-21 12:26:36 +0600

    7.4 Create Routes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const contactRouters = require("./contactRouters");
 
 const app = express();
 app.use(morgan("dev"));
+
+app.use("/contacts", contactRouters);
+
+app.get("*", function (req, res) {
+  res.send("<h1>Please, Use The Correct Routes</h1>");
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
