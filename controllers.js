76fd647a3c05fb6fba7fde32c34fd61bef0bd96d
Alvi Hasan <livealvi@gmail.com>
2021-11-22 16:52:50 +0600

    9.12 Query Contacts

--- controllers.js
+++ controllers.js
@@ -1,8 +1,27 @@
 const Contact = require("./Contact");
 
-exports.getAllConnect = (req, res) => {};
+exports.getAllConnect = (req, res) => {
+  Contact.find()
+    .then((contacts) => {
+      res.json(contacts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ message: "Error Occurred" });
+    });
+};
 
-exports.getSingleConnect = (req, res) => {};
+exports.getSingleConnect = (req, res) => {
+  let { id } = req.params;
+  Contact.findById(id)
+    .then((contact) => {
+      res.json(contact);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ message: "Error Occurred" });
+    });
+};
 
 exports.createContact = (req, res) => {
   let { name, phone, email } = req.body;
