832c5d2f5ac53a853618570bbaa229ea2e8eb5d5
Alvi Hasan <livealvi@gmail.com>
2021-11-23 10:58:52 +0600

    User Mode

---
+++ models/User.js
@@ -0,0 +1,34 @@
+//Name, Email, Password, Profile
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxLength: 30,
+      require: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    profile: {
+      type: Schema.Types.ObjectId,
+      ref: "Profile",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = model("User", userSchema);
+
+module.exports = User;
