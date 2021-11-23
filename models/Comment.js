51189ef945a4981882c575134f35605a273173aa
Alvi Hasan <livealvi@gmail.com>
2021-11-23 11:24:51 +0600

    Comment Model

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+// post, user, body, replies, error
+
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema(
+  {
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      require: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    body: {
+      type: String,
+      trim: true,
+      require: true,
+    },
+    replies: [
+      {
+        body: {
+          type: String,
+          require: true,
+        },
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          require: true,
+        },
+        createdAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = model("Comment", commentSchema);
+
+module.exports = Comment;
