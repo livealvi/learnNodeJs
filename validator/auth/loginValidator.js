b73b78479d80b66b90088635583bc606099e2c84
Alvi Hasan <livealvi@gmail.com>
2021-11-24 18:50:06 +0600

    fix

--- validator/auth/loginValidator.js
+++ validator/auth/loginValidator.js
@@ -2,12 +2,7 @@ const { body } = require("express-validator");
 const User = require("../../models/User");
 
 module.exports = [
-  body("email")
-    .isEmail()
-    .withMessage("Please provide a valid email address")
-    .normalizeEmail(),
+  body("email").isEmail().withMessage("email cannot be empty").normalizeEmail(),
 
-  body("password")
-    .isLength({ min: 5 })
-    .withMessage("your password must be at least 5 characters"),
+  body("password").isLength({ min: 5 }).withMessage("password cannot be empty"),
 ];
