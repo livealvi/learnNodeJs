fd87a9f1f7b0853e962b9b75d22041f95834ed56
Alvi Hasan <livealvi@gmail.com>
2021-11-24 12:40:26 +0600

    Signup Validator

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const { body } = require("express-validator");
+const userModel = require("../models/User");
 
 const {
   signupGetController,
@@ -8,8 +10,42 @@ const {
   logoutController,
 } = require("../controllers/authController");
 
+const signupValidator = [
+  body("username")
+    .isLength({ min: 2, max: 15 })
+    .withMessage("username must be between 2 and 15 characters")
+    .custom(async (username) => {
+      let user = await User.findOne({ username });
+      if (user) {
+        return Promise.reject("username already used");
+      }
+    })
+    .trim(),
+
+  body(email)
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .custom(async (email) => {
+      let user = await User.findOne({ email });
+      if (user) {
+        return Promise.reject("email already used");
+      }
+    })
+    .normalizeEmail(),
+
+  body(password)
+    .isLength({ min: 5 })
+    .withMessage("your password must be at least 5 characters"),
+
+  body(confirmPassword).custom((confirmPassword, { req }) => {
+    if (confirmPassword != req.body.password) {
+      throw new Error("password is not match");
+    }
+  }),
+];
+
 router.get("/signup", signupGetController);
-router.post("/signup", signupPostController);
+router.post("/signup", signupValidator, signupPostController);
 
 router.get("/login", loginGetController);
 router.post("/login", loginPostController);
