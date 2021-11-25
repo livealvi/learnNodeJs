410bbc390602f6c5f0f6eb9db217a1ee8844c8b1
Alvi Hasan <livealvi@gmail.com>
2021-11-25 22:08:26 +0600

    IsAuthenticated Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -16,3 +16,10 @@ exports.bindUserWithRequest = () => {
     }
   };
 };
+
+exports.isAuthenticated = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/auth/login");
+  }
+  next();
+};
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const { isAuthenticated } = require("../middleware/authMiddleware");
 
 const {
   dashboardGetController,
 } = require("../controllers/dashboardController");
 
-router.get("/", dashboardGetController);
+router.get("/", isAuthenticated, dashboardGetController);
 
 module.exports = router;
