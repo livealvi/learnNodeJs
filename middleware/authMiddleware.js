6d4e7d2543f3355c6252abfb96a24536cf7c7adb
Alvi Hasan <livealvi@gmail.com>
2021-11-25 22:33:51 +0600

    Unauthenticated Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -23,3 +23,10 @@ exports.isAuthenticated = (req, res, next) => {
   }
   next();
 };
+
+exports.isUnAuthenticated = (req, res, next) => {
+  if (req.session.isLoggedIn) {
+    return res.redirect("/dashboard");
+  }
+  next();
+};
