1434e008bce28f821a669635be0ce895f8704efc
Alvi Hasan <livealvi@gmail.com>
2021-11-27 15:53:25 +0600

    Separate Middleware File

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const flash = require("connect-flash");
 const config = require("config");
 
-// Import Route
+const setMiddleware = require("./middleware/middleware");
 const setRoutes = require("./routes/routes");
-
-// Import Middleware
-const { bindUserWithRequest } = require("./middleware/authMiddleware");
-const setLocals = require("./middleware/setLocals");
 
 const MONGODB_URI = `mongodb+srv://${config.get("db-username")}:${config.get(
   "db-password"
 )}@cluster0.amhkf.mongodb.net/express-blog?retryWrites=true&w=majority`;
-
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-  saveUninitialized: false,
-  expires: 1000 * 60 * 60 * 2,
-});
 
 const app = express();
 
@@ -31,23 +16,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-//Middleware array
-const middleware = [
-  express.static("public"),
-  express.urlencoded({ extended: true }),
-  express.json(),
-  session({
-    secret: config.get("secret"),
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  }),
-  bindUserWithRequest(),
-  setLocals(),
-  flash(),
-];
-
-app.use(middleware);
+// Using Middleware from Middleware Dir
+setMiddleware(app);
 
 // Using Routes from Route Dir
 setRoutes(app);
