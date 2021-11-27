5c024c85ff3323102b9445f7828f4f43d73debbe
Alvi Hasan <livealvi@gmail.com>
2021-11-27 19:04:25 +0600

    Setup Local Storage : Multer

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,11 @@
+const multer = require("multer");
+const plath = require("plath");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
+});
